7ee1f82e34b2526b601e12cb93195f8a29da56c3
Nicholas Dongoske <nickdongoske@gmail.com>
2020-02-08 23:27:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,21 @@
-# require modules here
+require 'YAML'
+require 'pry'
 
 def load_library
-  # code goes here
+   emoticons = YAML.load_file(file_path)
+
+  # Hash must use the => syntax to pass tests.
+  # "get_meaning": {} will not pass the test suite.
+  return_hash = {
+    "get_meaning" => {},
+    "get_emoticon" => {},
+  }
+
+  emoticons.each do |meaning, translations|
+    return_hash["get_meaning"][translations[1]] = meaning
+    return_hash["get_emoticon"][translations[0]] = translations[1]
+  end
+  return_hash
 end
 
 def get_japanese_emoticon
